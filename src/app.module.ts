41c5022b9bc330b6fd54c031f693925dc8a3a0d3
dihas ananda <dihasananda@gmail.com>
2023-10-08 20:41:10 +0700

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FilmsModule } from './films/films.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [FilmsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), FilmsModule],
 })
 export class AppModule {}
